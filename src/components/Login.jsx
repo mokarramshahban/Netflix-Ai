f8bd49476ad4b41bc6574d296969049960cb7674
Mokarram Shahban <freedomshahban07@gmail.com>
2026-03-10 14:07:35 +0530

    feat: setup signIn/signUp validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import BG_IMAGE from "../assets/images/login-bg.jpg";
 import Header from "./Header";
+import { checkValidDataSignIn, checkValidDataSignUp } from "../utils/validate";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const name = useRef(null)
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleButtonClick = () => {
+    // validate email & password
+    if(isSignIn){
+    const message = checkValidDataSignIn(email.current.value, password.current.value);
+        setErrorMessage(message);
+    }
+
+    if(!isSignIn) {
+      const message = checkValidDataSignUp(name.current.value, email.current.value, password.current.value);
+      setErrorMessage(message);
+    }
+  };
 
   const handleSignInSignUpToggle = () => {
     setIsSignIn(!isSignIn);
@@ -16,36 +33,50 @@ const Login = () => {
       </div>
       <div className="bg-black/60 h-full w-full absolute">
         <div className="p-12 absolute bg-black/70 text-stone-50 w-full sm:w-6/12 md:w-4/12 mx-auto my-45 left-0 right-0 rounded-lg">
-          <form action="" className="flex flex-col items-center">
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            action=""
+            className="flex flex-col items-center"
+          >
             <h1 className="text-white text-3xl m-2 w-full text-left font-bold">
               {isSignIn ? "Sign In" : "Sign Up"}
             </h1>
             {!isSignIn && (
               <input
+                ref={name}
                 className="m-4 p-3 bg-gray-700 w-full rounded-md"
                 type="text"
                 placeholder="Full Name"
               />
             )}
             <input
+              ref={email}
               className="m-4 p-3 bg-gray-700 w-full rounded-md"
               type="text"
               placeholder="Email"
             />
             <input
+              ref={password}
               className="m-4 p-3 bg-gray-700 w-full rounded-md"
               type="password"
               placeholder="Password"
             />
-            <button className="bg-red-700 m-4 p-3 w-full rounded-lg hover:scale-95 transition-all duration-200">
+            <p className="text-red-600 font-bold text-lg py-2">
+              {errorMessage}
+            </p>
+            <button
+              onClick={handleButtonClick}
+              className="bg-red-700 m-4 p-3 w-full rounded-lg hover:scale-95 transition-all duration-200"
+            >
               {isSignIn ? "Sign In" : "Sign Up"}
             </button>
             <p
               className="py-4 cursor-pointer hover:scale-105 transition-all duration-200"
               onClick={handleSignInSignUpToggle}
             >
-               {isSignIn ? "New to Netflix? Sign up now" : "Already registered? Sign in now"}
-              
+              {isSignIn
+                ? "New to Netflix? Sign up now"
+                : "Already registered? Sign in now"}
             </p>
           </form>
         </div>
