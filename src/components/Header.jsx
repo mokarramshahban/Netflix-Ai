3003b723bdd59c36b368208429d0a12ad4b613cc
Mokarram Shahban <freedomshahban07@gmail.com>
2026-03-16 23:43:51 +0530

    feat: signout option

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import LOGO from '../assets/images/logo.png'
+import React from "react";
+import LOGO from "../assets/images/logo.png";
+import USER_ICON from "../assets/images/userIcon.jpg";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase"
 const Header = () => {
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/")
+      })
+      .catch((error) => {
+        navigate("/error")
+      });
+  };
   return (
-    <div className='absolute z-9 w-full bg-linear-330 from-black'>
-      <img className='w-35 mx-10' src={LOGO} alt="" />
+    <div className="absolute z-9 w-full bg-linear-330 from-black flex justify-between">
+      <img className="w-35 mx-10" src={LOGO} alt="" />
+      <div className="flex items-center mx-3">
+        <img className="w-15 h-15 m-2 rounded-sm" src={USER_ICON} alt="" />
+        <button
+          onClick={handleSignOut}
+          className="text-white border border-white h-5/12 px-3 rounded-sm mx-2 hover:scale-95 transition-all duration-200"
+        >
+          Sign out
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
